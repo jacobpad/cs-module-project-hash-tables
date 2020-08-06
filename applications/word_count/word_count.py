9f71ce84bda537115f344b285aa01292b254f100
jacobpad <jacobpad@gmail.com>
2020-08-05 20:35:38 -0700

    word_count completed

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,10 +1,25 @@
-def word_count(s):
-    # Your code here
+print('-'*75, '\n')
 
+
+def word_count(s):
+    cache = {}
+    ignore_punc = '":;,.-+=/\|[]{}()*^&'
+
+    # Chain strip of special characters, including " "
+    s = s.lower().replace('\r', ' ').replace('\n', ' ').replace('\t', ' ').split()
+
+    for i in s:
+        i = i.strip(ignore_punc)
+        if i not in cache and i != "":
+            cache[i] = 1
+        elif i != "":
+          cache[i] += 1
+    return cache
 
 
 if __name__ == "__main__":
     print(word_count(""))
     print(word_count("Hello"))
     print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
-    print(word_count('This is a test of the emergency broadcast network. This is only a test.'))
+    print(word_count(
+        'This is a test of the emergency broadcast network. This is only a test.'))
